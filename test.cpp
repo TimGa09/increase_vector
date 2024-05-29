5375ba162cd242177d35e9d1db107034531e57ef
TimGa09 <167896293+TimGa09@users.noreply.github.com>
2024-05-29 16:48:57 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,4 @@
+﻿#include <pch.h>
+#include "C:\C++\Повторение 4 модуль\Header.cpp"
+Test(TestIncrease_vector, test1)
+EXPECT_TRUE(increase_vector({ 1,2,3 }, 1) == {2, 3, 4});
---
+++ Повторение 4 модуль.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+vector <int> increase_vector(vector <int> vec, int n) {
+    for (int i = 0; i < vec.size(); i++) {
+        vec[i] = n + vec[i];
+    }
+    return vec;
+}
+int main() 
+{
+    int n;
+    cin >> n;
+    vector <int> vect{1,2,3,4,5,6,7,8,9,10};
+    vect = increase_vector(vect,n);
+    for (int i = 0; i < vect.size(); i++) {
+        cout << vect[i] << endl;
+    }
+}
